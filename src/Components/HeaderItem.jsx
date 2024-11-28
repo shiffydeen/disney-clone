eadbd511fe7463695423a595d0145219cd719276
Sheriff Aderombi <ayaderombi@gmail.com>
2024-11-28 23:43:41 +0100

    update

---
+++ src/Components/HeaderItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderItem = ({name, Icon}) => {
+  return (
+    <div className="text-white flex items-center gap-3 text-[15px] font-semibold cursor-pointer hover:underline underline-offset-8">
+        {/* {icon()} */}
+        <Icon />
+      <h2 className="hidden md:block">{name}</h2>
+    </div>
+  )
+}
+
+export default HeaderItem
